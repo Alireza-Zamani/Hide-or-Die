3d08eb646c975cc6bdf21af7290a127794a72d1a
MH2B <hasan.helali14@gmail.com>
2020-12-22 20:03:36 +0330

    Player Container Resized

---
+++ Hide Or Die/Assets/Scripts/SpriteSorter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor;
+[InitializeOnLoad]
+
+
+class SpriteSorter
+{
+    static SpriteSorter()
+    {
+        Initialize();
+    }
+
+    [RuntimeInitializeOnLoadMethod]
+    static void Initialize()
+	{
+        GraphicsSettings.transparencySortMode = TransparencySortMode.CustomAxis;
+        GraphicsSettings.transparencySortAxis = new Vector3(0.0f, 1.0f, 1.0f);
+	}
+
+
+
+}
